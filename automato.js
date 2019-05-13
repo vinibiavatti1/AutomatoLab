5bbc19f290b1d00dd5d2dc2907a19b135f41ed52
Vinicius Reif <vinicius.reif@senior.com.br>
2019-05-13 17:59:37 -0300

    Criado tooltips

--- automato.js
+++ automato.js
@@ -2,6 +2,10 @@ var ferramentas = {
     pincel: 0,
     borracha: 1
 }
+var temas = {
+    dia: 0,
+    noite: 1
+}
 var pixeis = [];
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -17,17 +21,43 @@ var corGradeDivisor = "rgba(0,0,0,0.2)";
 var maxZoom = 20;
 var valorGradeDivisor = 5;
 var corPixel = "rgb(0,200,0)";
+var corCursor = "rgba(0,0,0,0.8)";
+var mostrarGrade = true;
+var velocidadeSimulacao = 5;
 
 /**
  * Iniciar
  */
-function init() {
+$(document).ready(function() {
     ctx.translate(0.5, 0.5);
     ctx.scale(1, 1);
     zoom(0);
     renderizar();
+    $(".indicador-velocidade").html(velocidadeSimulacao);
+    $("#tamanho-cursor").attr("src", `recursos/tamanhos_cursor/${tamanhoCursor}.png`);
+    $("#tamanho-cursor-2").attr("src", `recursos/tamanhos_cursor/${tamanhoCursor}.png`);
+});
+
+/**
+ * Mostrar grade
+ * @param {*} mostrar 
+ */
+function exibirGrade(mostrar) {
+    mostrarGrade = mostrar;
+    renderizar();
 }
-init();
+
+/**
+ * Definir velocidade de simulação
+ * @param {*} add 
+ */
+function addVelocidadeSimulacao(add) {
+    if(velocidadeSimulacao + add < 0 || velocidadeSimulacao + add > 10) {
+        return;
+    }
+    velocidadeSimulacao += add;
+    $(".indicador-velocidade").html(velocidadeSimulacao);
+}
 
 /**
  * Selecionar Ferramenta
@@ -97,7 +127,7 @@ function desenharCursor(event) {
 
     renderizar();
 
-    ctx.fillStyle = "black";
+    ctx.fillStyle = corCursor;
     for (var i = 0; i < tamanhoCursor; i++) {
         for (var j = 0; j < tamanhoCursor; j++) {
             ctx.fillRect((obj.x + i) * tamanhoBloco, (obj.y + j) * tamanhoBloco, tamanhoBloco, tamanhoBloco);
@@ -160,6 +190,9 @@ function desenharFundo() {
  * Desenhar grade
  */
 function desenharGrade() {
+    if(!mostrarGrade) {
+        return;
+    }
     var contador = 1;
     for (var i = 0; i < tamanho; i += tamanhoBloco) {
         ctx.beginPath();
@@ -178,4 +211,41 @@ function desenharGrade() {
         }
         contador++;
     }
+}
+
+/**
+ * Mostrar Tooltip
+ * @param {*} id 
+ */
+function mostrarTooltip(event, id) {
+    var top = $(event.target).position().top;
+    $("#" + id).show();
+    $("#" + id).css("top", top + "px");
+}
+
+/**
+ * Ocultar Tooltip
+ * @param {*} id 
+ */
+function ocultarTooltip(event, id) {
+    $("#" + id).hide();
+}
+
+/**
+ * Mudar Tema
+ * @param {*} tema 
+ */
+function mudarTema(tema) {
+    if(tema == temas.dia) {
+        corFundo = "white";
+        corGrade = "rgba(0,0,0,0.1)";
+        corGradeDivisor = "rgba(0,0,0,0.2)";
+        corCursor = "rgba(0,0,0,0.8)";
+    } else if(tema == temas.noite) {
+        corFundo = "rgba(0,0,0,0.8)";
+        corGrade = "rgba(255,255,255,0.1)";
+        corGradeDivisor = "rgba(255,255,255,0.2)";
+        corCursor = "rgba(255,255,255,0.8)";
+    }
+    renderizar();
 }
