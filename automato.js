3374ff8da397174804c1d61fcbfb8ba709fd4cee
Vinicius Reif <vinicius.reif@senior.com.br>
2019-05-15 13:13:09 -0300

    Criado wireworld

--- automato.js
+++ automato.js
@@ -25,7 +25,9 @@ var automatosBidimensionais = [
 ]
 var ferramentas = {
     pincel: 0,
-    borracha: 1
+    borracha: 1,
+    fio: 2,
+    energia: 3
 }
 var temas = {
     dia: 0,
@@ -33,7 +35,14 @@ var temas = {
 }
 var tipoSimulacao = {
     unidimensional: 0, 
-    bidimensional: 1
+    bidimensional: 1,
+    wireworld: 2
+}
+var wireworld = {
+    vazio: 2,
+    fio: 3,
+    cabeca: 4,
+    calda: 5
 }
 
 /**
@@ -47,15 +56,11 @@ var tamanhoCursor = 1;
 var tamanhoMaxCursor = 5;
 var ferramenta = ferramentas.pincel;
 var tamanhoBloco = 10;
-var qtdBlocos = 50;
+var qtdQuadros = 15;
+var qtdBlocos = qtdQuadros * 5;
 var tamanho = tamanhoBloco * qtdBlocos;
-var corFundo = "white";
-var corGrade = "rgba(0,0,0,0.1)";
-var corGradeDivisor = "rgba(0,0,0,0.2)";
 var maxZoom = 20;
 var valorGradeDivisor = 5;
-var corPixel = "rgb(0,200,0)";
-var corCursor = "rgba(0,0,0,0.8)";
 var mostrarGrade = true;
 var velocidadeSimulacao = 10;
 var idRegraUnidimensional = 30;
@@ -67,28 +72,138 @@ var tipoSelecionado = tipo;
 var automato = automatosBidimensionais[0];
 
 /**
+ * Cores
+ */
+var corFundo = "white";
+var corGrade = "rgba(0,0,0,0.1)";
+var corGradeDivisor = "rgba(0,0,0,0.2)";
+var corCelula = "rgb(0,200,0)";
+var corCursor = "rgba(0,0,0,0.8)";
+var corFio = "rgb(255,215,0)";
+var corCabeca = "rgb(0,128,255)";
+var corCalda = "rgb(255,64,0)";
+
+/**
  * Construtor
  */
 $(document).ready(function() {
     ctx.translate(0.5, 0.5);
     ctx.scale(1, 1);
-    criarMapa();
-    zoom(0);
-    renderizar();
     $(".indicador-velocidade").html(velocidadeSimulacao);
     $("#tamanho-cursor").attr("src", `recursos/tamanhos_cursor/${tamanhoCursor}.png`);
     $("#tamanho-cursor-2").attr("src", `recursos/tamanhos_cursor/${tamanhoCursor}.png`);
-    $("#corIcone").css("color", corPixel);
+    $("#corIcone").css("color", corCelula);
+    iniciarSelects();
+    preencherRegraBidimensional();
+    definirAutomato();
+});
+
+/**
+ * Definir autômato
+ */
+function definirAutomato() {
+    pausado = true;
     mudarIconesIniciar();
+    tipo = tipoSelecionado;
+
+    qtdQuadros = $("#qtdQuadros").val();
+    qtdBlocos = qtdQuadros  * 5;
+    tamanho = tamanhoBloco * qtdBlocos;
+    atualizarTamanhoCanvas(tamanho);
+    
+    if(tipo == tipoSimulacao.unidimensional) {
+        idRegraUnidimensional = $("#seletorRegraUnidimensional").val();
+        regraUnidimensional = obterRegraUnidimensional(idRegraUnidimensional);
+        corCelula = "black";
+        $("#corCelulaInput").val(corCelula);
+        $("#corIcone").css("color", corCelula);
+        $("#nomeAutomato").html("Unidimensional / Regra " + idRegraUnidimensional);
+        $("#botaoAutomato").attr("Unidimensional / Regra " + idRegraUnidimensional);
+        ferramenta = ferramentas.pincel;
+    } else if(tipo == tipoSimulacao.bidimensional) {
+        var automatoId = $("#seletorRegraBidimensional").val();
+        for(var i = 0; i < automatosBidimensionais.length; i++) {
+            if(automatosBidimensionais[i].id == automatoId) {
+                automato = automatosBidimensionais[i];
+            }
+        }
+        corCelula = automato.cor;
+        $("#corCelulaInput").val(corCelula);
+        $("#corIcone").css("color", corCelula);
+        $("#nomeAutomato").html(automato.nome);
+        $("#botaoAutomato").attr("title", automato.nome + " " + automato.regra.nome);
+        ferramenta = ferramentas.pincel;
+    } else if(tipo == tipoSimulacao.wireworld) {
+        $("#corCelulaInput").val(corCelula);
+        $("#corIcone").css("color", corCelula);
+        $("#nomeAutomato").html("Wireworld");
+        $("#botaoAutomato").attr("Wireworld");
+        $("#corFioInput").val(corFio);
+        $("#corIconeFio").css("color", corFio);
+        $("#corCabecaInput").val(corCabeca);
+        $("#corIconeCabeca").css("color", corCabeca);
+        $("#corCaldaInput").val(corCalda);
+        $("#corIconeCalda").css("color", corCalda);
+        ferramenta = ferramentas.fio;
+    }
+    mudarBotoes();
+    criarMapa();
+    renderizar();
+}
+
+/**
+ * Mudar botões conforme tipo
+ */
+function mudarBotoes() {
+    if(tipo == tipoSimulacao.wireworld) {
+        $("#botaoFio").show();
+        $("#botaoEnergia").show();
+        $("#botaoPincel").hide();
+        $("#botaoCorCabeca").show();
+        $("#botaoCorFio").show();
+        $("#botaoCorCalda").show();
+        $("#botaoCorCelula").hide();
+    } else {
+        $("#botaoFio").hide();
+        $("#botaoEnergia").hide();
+        $("#botaoPincel").show();
+        $("#botaoCorCabeca").hide();
+        $("#botaoCorFio").hide();
+        $("#botaoCorCalda").hide();
+        $("#botaoCorCelula").show();
+    }
+}
+
+/**
+ * Atribuir valores aos selects
+ */
+function iniciarSelects() {
+    var selected = "";
     for(var i = 0; i < 256; i++) {
-        $("#seletorRegraUnidimensional").append(`<option value="${i}">Regra ${i}</option>`);
+        if(i == 30) {
+            selected = "selected";
+        } else {
+            selected = "";
+        }
+        var negrito = "";
+        if([30, 90, 110].includes(i)) {
+            negrito = "negrito";
+        }
+        $("#seletorRegraUnidimensional").append(`<option value="${i}" ${selected} class='${negrito}'>Regra ${i}</option>`);
     }
     for(var i = 0; i < automatosBidimensionais.length; i++) {
         var automato = automatosBidimensionais[i];
         $("#seletorRegraBidimensional").append(`<option value="${automato.id}">${automato.nome + " " + automato.regra.nome}</option>`);
     }
-    preencherRegraBidimensional();
-});
+    for(var i = 1; i <= 100; i++) {
+        if(i == 15) {
+            selected = "selected";
+        } else {
+            selected = "";
+        }
+        $("#qtdQuadros").append(`<option value="${i}" ${selected}>${i} quadro(s)</option>`);
+    }
+}
 
 /**
  * Mudar tipo de simulação
@@ -145,25 +260,89 @@ function mudarIconesIniciar() {
 function iniciar() {
     pausado = !pausado;
     mudarIconesIniciar();
+    clearInterval(interval);
     if(tipo == tipoSimulacao.unidimensional) {
         iniciarSimulacaoUnidimensional();
-    } else {
+    } else if(tipo == tipoSimulacao.bidimensional) {
         iniciarSimulacaoBidimensional();
+    } else if(tipo == tipoSimulacao.wireworld) {
+        iniciarSimulacaoWireWorld();
     }
     $("#botaoIniciar").addClass("botao-iniciar-clicado");
+}
+
+/**
+ * Simulação WireWorld
+ */
+function iniciarSimulacaoWireWorld() {
+    var delay = (10 - velocidadeSimulacao) * 25;
+    interval = setInterval(function() {
+        if(!pausado) {
+            var mapaAux = copiarMatriz(mapa);
+            for(var i = 0; i < qtdBlocos; i++) {
+                for(var j = 0; j < qtdBlocos; j++) {
+                    if(mapa[i][j] == wireworld.cabeca) {
+                        mapaAux[i][j] = wireworld.calda;
+                    } else if(mapa[i][j] == wireworld.calda) {
+                        mapaAux[i][j] = wireworld.fio;
+                    } else if(mapa[i][j] == wireworld.fio) {
+                        var qtd = getQtdVizinhos(wireworld.cabeca, i, j);
+                        if([1, 2].includes(qtd)) {
+                            mapaAux[i][j] = wireworld.cabeca;
+                        }
+                    } 
+                }
+            }
+            mapa = copiarMatriz(mapaAux);
+            renderizar();
+        }
+    }, delay);
+}
+
+/**
+ * Simulação Unidimensional
+ */
+function iniciarSimulacaoUnidimensional() {
+    var i = 1;
+    var j = 0;
+    var delay =  (10 - velocidadeSimulacao) * 10;
+    interval = setInterval(function() {
+        if(!pausado) {
+            for(var k = 0; k < 10; k++){
+                if(i > qtdBlocos -1) {
+                    clearInterval(interval);
+                    return;
+                } 
+                var p1 = mapa[i - 1][j - 1] ;
+                var p2 = mapa[i - 1][j];
+                var p3 = mapa[i - 1][j + 1];
+                var celula = regraUnidimensional[p1 + "" + p2 + "" + p3];
+                if(celula == 1) {
+                    mapa[i][j] = celula;
+                }
+                j++;
+                if(j > qtdBlocos) {
+                    j = 0;
+                    i++;
+                }
+                
+            }
+            renderizar();
+        }
+    }, delay);
 }
 
 /**
  * Simulação Bidimensional
  */
 function iniciarSimulacaoBidimensional() {
-    var delay =  (10 - velocidadeSimulacao) * 100;
+    var delay = (10 - velocidadeSimulacao) * 25;
     interval = setInterval(function() {
         if(!pausado) {
             var mapaAux = copiarMatriz(mapa);
             for(var i = 0; i < qtdBlocos; i++) {
                 for(var j = 0; j < qtdBlocos; j++) {
-                    var qtd = getQtdVizinhos(i, j);
+                    var qtd = getQtdVizinhos(1, i, j);
                     if(mapa[i][j] == 1) {
                         if(!automato.regra.step.includes(qtd)) {
                             mapaAux[i][j] = 0;
@@ -186,55 +365,55 @@ function iniciarSimulacaoBidimensional() {
  * @param {*} i 
  * @param {*} j 
  */
-function getQtdVizinhos(i, j) {
+function getQtdVizinhos(valor, i, j) {
     var qtd = 0;
     
     x = i -1;
     y = j -1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i -1;
     y = j;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i -1;
     y = j +1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i;
     y = j -1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i;
     y = j +1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i +1;
     y = j -1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i +1;
     y = j;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
 
     x = i +1;
     y = j +1;
     if(existePosicao(x, y)) {
-        qtd += mapa[x][y];
+        qtd += mapa[x][y] == valor ? 1 : 0;
     }
     return qtd;
 }
@@ -246,72 +425,6 @@ function getQtdVizinhos(i, j) {
  */
 function existePosicao(i, j) {
     return mapa[i] != undefined && mapa[i][j] != undefined;
-}
-
-/**
- * Simulação Unidimensional
- */
-function iniciarSimulacaoUnidimensional() {
-    var i = 1;
-    var j = 0;
-    label:
-    for(var ii = 1; ii < qtdBlocos -1; ii++) {
-        for(var jj = 0; jj < qtdBlocos -1; jj++) {
-            if(mapa[ii][jj] == 1) {
-                i = ii;
-                j = jj;
-                break label;
-            }
-        }
-    }
-    var delay =  (10 - velocidadeSimulacao) * 10;
-    interval = setInterval(function() {
-        if(!pausado) {
-            for(var k = 0; k < 10; k++){
-                if(i > qtdBlocos -1) {
-                    clearInterval(interval);
-                    return;
-                } 
-                var p1 = mapa[i - 1][j - 1] ;
-                var p2 = mapa[i - 1][j];
-                var p3 = mapa[i - 1][j + 1];
-                var celula = regraUnidimensional[p1 + "" + p2 + "" + p3];
-                if(celula == 1) {
-                    mapa[i][j] = celula;
-                }
-                j++;
-                if(j > qtdBlocos) {
-                    j = 0;
-                    i++;
-                }
-                renderizar();
-            }
-        }
-    }, delay);
-}
-
-/**
- * Definir autômato
- */
-function definirAutomato() {
-    pausado = true;
-    mudarIconesIniciar();
-    tipo = tipoSelecionado;
-    idRegraUnidimensional = $("#seletorRegraUnidimensional").val();
-    regraUnidimensional = obterRegraUnidimensional(idRegraUnidimensional);
-    var automatoId = $("#seletorRegraBidimensional").val();
-    for(var i = 0; i < automatosBidimensionais.length; i++) {
-        if(automatosBidimensionais[i].id == automatoId) {
-            automato = automatosBidimensionais[i];
-        }
-    }
-    $("#corPixelInput").val(automato.cor);
-    $("#corIcone").css("color", automato.cor);
-    $("#nomeAutomato").html(automato.nome);
-    $("#botaoAutomato").attr("title", automato.nome + " " + automato.regra.nome);
-    corPixel = automato.cor;
-    criarMapa();
-    renderizar();
 }
 
 /**
@@ -343,16 +456,64 @@ function resetar() {
 /**
  * Abrir input de cor do pixel
  */
-function inputCorPixel() {
-    $("#corPixelInput").click();
+function inputCorCelula() {
+    $("#corCelulaInput").click();
 }
 
 /**
- * Mudar cor do pixel
+ * Abrir input de cor do fio
  */
-function mudarCorPixel() {
-    corPixel = $("#corPixelInput").val();
-    $("#corIcone").css("color", corPixel);
+function inputCorFio() {
+    $("#corFioInput").click();
+}
+
+/**
+ * Abrir input de cor do cabeca
+ */
+function inputCorCabeca() {
+    $("#corCabecaInput").click();
+}
+
+/**
+ * Abrir input de cor do calda
+ */
+function inputCorCalda() {
+    $("#corCaldaInput").click();
+}
+
+/**
+ * Mudar cor da celula
+ */
+function mudarCorCelula() {
+    corCelula = $("#corCelulaInput").val();
+    $("#corIcone").css("color", corCelula);
+    renderizar();
+}
+
+/**
+ * Mudar cor do fio
+ */
+function mudarCorFio() {
+    corFio = $("#corFioInput").val();
+    $("#corIconeFio").css("color", corFio);
+    renderizar();
+}
+
+/**
+ * Mudar cor da cabeça
+ */
+function mudarCorCabeca() {
+    corCabeca = $("#corCabecaInput").val();
+    $("#corIconeCabeca").css("color", corCabeca);
+    renderizar();
+}
+
+/**
+ * Mudar cor da calda
+ */
+function mudarCorCalda() {
+    corCalda = $("#corCaldaInput").val();
+    $("#corIconeCalda").css("color", corCalda);
     renderizar();
 }
 
@@ -387,7 +548,6 @@ function obterRegraUnidimensional(id) {
     for(var i = bin.length; i < 8; i++) {
         bin = "0" + bin;
     }
-    console.log(bin);
     var regra = {
         "000": bin[7],
         "001": bin[6],
@@ -458,16 +618,17 @@ function desenharPixel(event) {
                 "x": (Math.round((x - rect.left) / tamanhoBloco)) + i,
                 "y": (Math.round((y - rect.top) / tamanhoBloco)) + j
             };
+            if(tipo == tipoSimulacao.wireworld && ferramenta == ferramentas.energia && mapa[obj.y][obj.x] != wireworld.fio) {
+                continue;
+            }
             if (ferramenta == ferramentas.borracha) {
                 mapa[obj.y][obj.x] = 0;
-                /*for(var k = 0; k < pixeis.length; k++) {
-                    if(pixeis[k].x == obj.x && pixeis[k].y == obj.y) {
-                        pixeis.splice(k, 1);
-                        break;
-                    }
-                }*/
             } else if(ferramenta == ferramentas.pincel) {
                 mapa[obj.y][obj.x] = 1;
+            } else if(ferramenta == ferramentas.fio) {
+                mapa[obj.y][obj.x] = wireworld.fio;
+            } else if(ferramenta == ferramentas.energia) {
+                mapa[obj.y][obj.x] = wireworld.cabeca;
             }
 
         }
@@ -504,17 +665,22 @@ function desenharCursor(event) {
  * Imprimir pixeis
  */
 function desenharPixeis() {
-    ctx.fillStyle = corPixel;
+    ctx.fillStyle = corCelula;
     for (var i = 0; i < mapa.length; i++) {
         for (var j = 0; j < mapa.length; j++) {
-            if(mapa[i][j] == 1) {
+            if(mapa[i][j] > 0) {
+                if(mapa[i][j] == wireworld.fio) {
+                    ctx.fillStyle = corFio;
+                } else if(mapa[i][j] == wireworld.cabeca) {
+                    ctx.fillStyle = corCabeca;
+                } else if(mapa[i][j] == wireworld.calda) {
+                    ctx.fillStyle = corCalda;
+                }
                 ctx.fillRect(j * tamanhoBloco, i * tamanhoBloco, tamanhoBloco, tamanhoBloco);
             }
+            
         }
     }
-    /*for (var i = 0; i < pixeis.length; i++) {
-        ctx.fillRect(pixeis[i].x * tamanhoBloco, pixeis[i].y * tamanhoBloco, tamanhoBloco, tamanhoBloco);
-    }*/
 }
 
 /**
@@ -527,8 +693,15 @@ function zoom(par) {
     }
     tamanhoBloco += par;
     tamanho = tamanhoBloco * qtdBlocos;
-    ctx.canvas.width = tamanho;
-    ctx.canvas.height = tamanho;
+    atualizarTamanhoCanvas(tamanho);
+}
+
+/**
+ * Atualizar tamanho do canvas
+ */
+function atualizarTamanhoCanvas(tam) {
+    ctx.canvas.width = tam;
+    ctx.canvas.height = tam;
     renderizar();
 }
 
@@ -614,8 +787,8 @@ function mudarTema(tema) {
         corCursor = "rgba(0,0,0,0.8)";
     } else if(tema == temas.noite) {
         corFundo = "rgba(0,0,0,0.8)";
-        corGrade = "rgba(255,255,255,0.1)";
-        corGradeDivisor = "rgba(255,255,255,0.2)";
+        corGrade = "rgba(64,64,64,0.6)";
+        corGradeDivisor = "rgba(64,64,64,1)";
         corCursor = "rgba(255,255,255,0.8)";
     }
     renderizar();
